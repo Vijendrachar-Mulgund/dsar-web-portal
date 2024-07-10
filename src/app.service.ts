db41f14033add977addfd94c2ee869b9d6803b60
Vijendrachar M <44837786+Vijendrachar-Mulgund@users.noreply.github.com>
2024-07-10 23:06:38 +0100

    Ollama is integrated 🚀

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Get, Injectable } from '@nestjs/common';
+import Ollama from 'ollama';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  async getHello(): Promise<any> {
+    const response = await Ollama.chat({
+      model: 'llama3',
+      messages: [{ role: 'user', content: 'Describe YOLOv8 Model' }],
+    });
+
+    console.log('Response -> ', response);
+
+    return response.message.content;
   }
 }
